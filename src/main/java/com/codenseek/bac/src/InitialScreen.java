64f6a94787599035d4406739a654b8f40148b070
yu-jin-song <yujinsong.tech@gmail.com>
2024-09-01 22:42:37 +0900

    feat: Apply input filter and GbcUtil

--- src/main/java/com/codenseek/bac/src/InitialScreen.java
+++ src/main/java/com/codenseek/bac/src/InitialScreen.java
@@ -1,11 +1,12 @@
 package com.codenseek.bac.src;
 
-import com.codenseek.bac.src.util.GameKind;
-import com.codenseek.bac.src.util.Constants;
-import com.codenseek.bac.src.util.MessageUtils;
-import com.codenseek.bac.src.util.Messages;
+import com.codenseek.bac.src.util.*;
+
+import static com.codenseek.bac.src.util.MessageUtils.*;
+import static com.codenseek.bac.src.util.GbcUtils.*;
 
 import javax.swing.*;
+import javax.swing.text.AbstractDocument;
 import java.awt.*;
 import java.util.Objects;
 
@@ -24,63 +25,59 @@ public class InitialScreen extends JPanel {
      * @param frame EntryFrame 객체
      */
     public InitialScreen(EntryFrame frame) {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new GridBagLayout());
+
+        // 기본 GridBagConstraints 설정
+        GridBagConstraints gbc = createDefaultGbc();
 
         /**
          * 게임 종류 지정
          */
-        // 패널
-        JPanel gameKindPanel = new JPanel();
-        gameKindPanel.setLayout(new BoxLayout(gameKindPanel, BoxLayout.Y_AXIS));
-        gameKindPanel.setBorder(Constants.COMMON_EMPTY_BORDER);
-
         // 레이블
         JLabel gameKindLabel = new JLabel("게임 종류를 선택하세요.", JLabel.CENTER);
-        gameKindLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        gameKindPanel.add(gameKindLabel);
+        gbc.gridy = 0;
+        gbc.insets.bottom = 2;
+        add(gameKindLabel, gbc);
 
         // 콤보박스(숫자/영어단어)
         gameKindCombo = new JComboBox<>(GameKind.values());
         gameKindCombo.setPreferredSize(new Dimension(80, 25));  // 크기 조절
-        gameKindCombo.setMaximumSize(gameKindCombo.getPreferredSize());
-        gameKindCombo.setAlignmentX(Component.CENTER_ALIGNMENT);
-        gameKindPanel.add(gameKindCombo);
-
-        add(gameKindPanel);
+        // 텍스트 가운데 정렬 설정
+        ((JLabel)gameKindCombo.getRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
+        gbc.gridy = 1;
+        gbc.insets.top = 2;
+        gbc.insets.bottom = getDefaultPadding();
+        add(gameKindCombo, gbc);
 
         /**
          * 자릿수 지정
          */
-        // 패널
-        JPanel wordLengthPanel = new JPanel();
-        wordLengthPanel.setLayout(new BoxLayout(wordLengthPanel, BoxLayout.Y_AXIS));
-        gameKindPanel.setBorder(Constants.COMMON_EMPTY_BORDER);
-
         // 레이블
-        JLabel wordLengthLabel = new JLabel("자릿수를 입력하세요.(3~7)", JLabel.CENTER);
-        wordLengthLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        wordLengthPanel.add(wordLengthLabel);
+        JLabel wordLengthLabel = new JLabel("자릿수를 입력하세요.(3 ~ 7)", JLabel.CENTER);
+        gbc.gridy = 2;
+        gbc.insets.top = getDefaultPadding();
+        gbc.insets.bottom = 2;
+        add(wordLengthLabel, gbc);
 
         // 자릿수를 입력받는 텍스트 필드
-        wordLengthField = new JTextField(2);
-        wordLengthField.setMaximumSize(wordLengthField.getPreferredSize());  // 최대 크기를 선호 크기로 제한
-        wordLengthField.setAlignmentX(Component.CENTER_ALIGNMENT);
-        wordLengthPanel.add(wordLengthField);
-
-        add(wordLengthPanel);
+        wordLengthField = new JTextField(3);
+        gbc.gridy = 3;
+        // 텍스트 가운데 정렬 설정
+        wordLengthField.setHorizontalAlignment(JTextField.CENTER);
+        // 한 자릿수만 입력 가능하도록 필터 설정
+        ((AbstractDocument) wordLengthField.getDocument()).setDocumentFilter(new InputFilters.PlaceValueFilter());
+        gbc.insets.top = 2;
+        gbc.insets.bottom = getDefaultPadding();
+        add(wordLengthField, gbc);
 
         /**
          * 확인 버튼
          */
-        // 패널
-        JPanel confirmPanel = new JPanel();
-        confirmPanel.setLayout(new BoxLayout(confirmPanel, BoxLayout.Y_AXIS));
-        confirmPanel.setBorder(Constants.COMMON_EMPTY_BORDER);
-
-        // 확인 버튼
         JButton confirmButton = new JButton(Constants.CHECK);
-        confirmButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(confirmButton);
+        gbc.gridy = 4;
+        gbc.insets.top = 20;
+        gbc.insets.bottom = getDefaultPadding();
+        add(confirmButton, gbc);
 
         confirmButton.addActionListener(e -> {
             try {
@@ -88,27 +85,27 @@ public class InitialScreen extends JPanel {
                 GameKind gameKind = (GameKind) gameKindCombo.getSelectedItem();
 
                 if (Objects.isNull(gameKind)) {
-                    MessageUtils.showErrorMessage(this, Messages.SELECT_GAME_KIND);
+                    showErrorMessage(this, Messages.SELECT_GAME_KIND);
                     return; // 게임 종류가 선택되지 않은 경우 메서드 종료
                 }
 
                 // 자릿수 입력 여부
                 String wordLengthText = wordLengthField.getText();
                 if(wordLengthText.trim().isEmpty()) {
-                    MessageUtils.showErrorMessage(this, Messages.ERROR_EMPTY_LENGTH);
+                    showErrorMessage(this, Messages.ERROR_EMPTY_LENGTH);
                     return;
                 }
 
                 // 자릿수 판단
                 int wordLength = Integer.parseInt(wordLengthText);
                 if(wordLength < 3 || wordLength > 7) {
-                    MessageUtils.showErrorMessage(this, Messages.INVALID_WORD_LENGTH);
+                    showErrorMessage(this, Messages.INVALID_WORD_LENGTH);
                 } else {
                     frame.startGame(gameKind, wordLength);
                 }
             } catch (NumberFormatException ex) {
                 // 숫자가 아닌 값을 입력한 경우 경고 메시지를 출력
-                MessageUtils.showErrorMessage(this, Messages.ERROR_ONLY_NUMBERS);
+                showErrorMessage(this, Messages.ERROR_ONLY_NUMBERS);
             }
         });
     }
